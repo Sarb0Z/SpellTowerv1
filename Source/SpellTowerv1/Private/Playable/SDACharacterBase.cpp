c091613ceec193f3d08c9f3eeee440a027fc1f79
Sarb0Z <93070907+Sarb0Z@users.noreply.github.com>
2022-11-29 17:06:52 +0500

    latest builds

--- Source/SpellTowerv1/Private/Playable/SDACharacterBase.cpp
+++ Source/SpellTowerv1/Private/Playable/SDACharacterBase.cpp
@@ -3,32 +3,63 @@
 
 #include "Playable/SDACharacterBase.h"
 
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/Controller.h"
+
 // Sets default values
 ASDACharacterBase::ASDACharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+
+	currentScore = 0;
+	speed = 1;
+	maxJumpHeight = 2;
+
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment(RootComponent);
+
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	cameraComp->SetupAttachment(springArmComp);
+
+	staticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	staticMeshComp->SetupAttachment(RootComponent);
+	staticMeshComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	staticMeshComp->SetCollisionProfileName(TEXT("PhysicsActor"));
+
 
 }
+
+
+
 
 // Called when the game starts or when spawned
 void ASDACharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ASDACharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	//staticMeshComp->OnComponentBeginOverlap.AddDynamic(this, &ASDACharacterBase::OnOverlapBegin);
 
 }
+
 
 // Called to bind functionality to input
 void ASDACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::StopJumping);
 
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASDACharacterBase::changeDirection);
+
+}
+
+void ASDACharacterBase::changeDirection(float angle) {
+	/*FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, angle);*/
+	AddMovementInput(FVector(angle, 0.0f, 0.0f), 1.0f, false);
 }
 
--- Source/SpellTowerv1/Public/Playable/SDACharacterBase.h
+++ Source/SpellTowerv1/Public/Playable/SDACharacterBase.h
@@ -6,6 +6,12 @@
 #include "GameFramework/Character.h"
 #include "SDACharacterBase.generated.h"
 
+
+class USpringArmComponent;
+class UCameraComponent;
+class UStaticMeshComponent;
+class UBoxComponent;
+
 UCLASS()
 class SPELLTOWERV1_API ASDACharacterBase : public ACharacter
 {
@@ -14,14 +20,43 @@ class SPELLTOWERV1_API ASDACharacterBase : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ASDACharacterBase();
+	int currentScore;
+	int maxJumpHeight;
+	float speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		USpringArmComponent* springArmComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		UCameraComponent* cameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")
+		UStaticMeshComponent* staticMeshComp;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")
+	//	UBoxComponent* collisionBox;
+
+
+	//UFUNCTION()
+	//	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//// declare overlap end function
+	//UFUNCTION()
+	//	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void jump();
+	void changeDirection(float angle);
+	void updateHighScore();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+	//// Called every frame
+	//virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
